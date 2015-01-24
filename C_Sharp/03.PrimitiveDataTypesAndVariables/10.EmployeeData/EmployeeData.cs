9df19af4b3b30e1e4227a86c0b56ff2daa0e136b
SPPetrov <stoyko_petrov@mail.bg>
2015-01-24 09:36:26 -0800

    add C# file 03

---
+++ C_Sharp/03.PrimitiveDataTypesAndVariables/10.EmployeeData/EmployeeData.cs
@@ -0,0 +1,30 @@
+﻿//Problem 10.	Employee Data
+//A marketing company wants to keep record of its employees. Each record would have the following characteristics:
+//•	First name
+//•	Last name
+//•	Age (0...100)
+//•	Gender (m or f)
+//•	Personal ID number (e.g. 8306112507)
+//•	Unique employee number (27560000…27569999)
+//Declare the variables needed to keep the information for a single employee using appropriate primitive data types.
+//Use descriptive names. Print the data at the console.
+
+using System;
+class EmployeeData
+{
+    static void Main()
+    {
+        string firstName = "Kaloqn";
+        string lastName = "Kaloqnov";
+        byte age = 24;
+        char gender = 'm';
+        ulong personalID = 8306112507;
+        ulong employeeNumber = 27569999;
+        Console.WriteLine("First name :"+firstName);
+        Console.WriteLine("Last name :" + lastName);
+        Console.WriteLine("Age :"+age);
+        Console.WriteLine("Gender :"+gender);
+        Console.WriteLine("Personal ID Number :"+personalID);
+        Console.WriteLine("Employee Number :"+employeeNumber);
+    }
+}
---
+++ C_Sharp/03.PrimitiveDataTypesAndVariables/11.BankAccountData/BankAccountData.cs
@@ -0,0 +1,30 @@
+﻿//Problem 11.	Bank Account Data
+//A bank account has a holder name (first name, middle name and last name), available amount of money (balance),
+//bank name, IBAN, 3 credit card numbers associated with the account.Declare the variables needed to keep 
+//the information for a single bank account using the appropriate data types and descriptive names.
+
+using System;
+class BankAccountData
+{
+    static void Main()
+    {
+        string firstName = "Kaloqn";
+        string middleName = "Petrov";
+        string lastName = "Kaloqnov";
+        decimal balance = 12345678.91m;
+        string bankName = "PIB";
+        string iban = "5689 PIB BG 5656 5565 2165";
+        string firstCardNumber = "4546899266";
+        string secondCardNumber = "1234561232";
+        string thirdCardNumber = "5454465464";
+        Console.WriteLine("First name :"+firstName);
+        Console.WriteLine("Middle name :"+ middleName);
+        Console.WriteLine("Last name :"+lastName);
+        Console.WriteLine("Amount of money : "+balance);
+        Console.WriteLine("bankName :"+bankName);
+        Console.WriteLine("iban :"+iban);
+        Console.WriteLine("FirstCardNumber : "+firstCardNumber);
+        Console.WriteLine("SecondCardNumber: "+secondCardNumber);
+        Console.WriteLine("thirdCardNumber : "+thirdCardNumber);
+    }
+}
